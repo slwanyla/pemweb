82dbc29bfdb82ca85b99915ccfb1c251f50af44d
salwa nayla khairunnisa <if23.salwakhairunnisa@mhs.ubpkarawang.ac.id>
2024-11-26 19:30:53 +0700

    index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ini halaman home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Ini halaman  login</h1>
@@ -13,6 +14,6 @@
         <button type="submit">Login</button>
     </from>
 
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
