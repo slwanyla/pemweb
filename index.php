8641ff2d79a73a4c63bab66d52d15bf050cd4ea4
salwa nayla khairunnisa <if23.salwakhairunnisa@mhs.ubpkarawang.ac.id>
2024-11-26 19:34:49 +0700

    index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>login</h1>
@@ -12,6 +13,6 @@
         <input type="password" name="password" placeholder="masukkan password anda">
         <input type="submit" value="login" name="submit">
     </form>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
